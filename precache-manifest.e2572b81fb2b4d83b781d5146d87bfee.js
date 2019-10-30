92576d69744149f0e3b56d2f5b9e5f8f6d87935b
Vishal Juneja <jvishal@thoughtworks.com>
2019-10-30 13:28:32 +0530

    Updates

---
+++ precache-manifest.e2572b81fb2b4d83b781d5146d87bfee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d53977dee59864dd3e077e7968cb1b",
+    "url": "/nse_sample/index.html"
+  },
+  {
+    "revision": "c2290aa67fae89c9c767",
+    "url": "/nse_sample/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1c117b92bc9ee3154419",
+    "url": "/nse_sample/static/js/2.5c7cbcc1.chunk.js"
+  },
+  {
+    "revision": "c2290aa67fae89c9c767",
+    "url": "/nse_sample/static/js/main.a5db458a.chunk.js"
+  },
+  {
+    "revision": "7ee33132eba413cb7bd7",
+    "url": "/nse_sample/static/js/runtime-main.8e865f14.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/nse_sample/static/media/logo.25bf045c.svg"
+  }
+]);
